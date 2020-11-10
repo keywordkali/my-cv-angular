a4deaaba1dc41e643727326a3fccf35c42edb116
Kali Correll <kalicorrell18@gmail.com>
2020-11-09 21:34:54 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-cv-angular';
+  name: string = "Kali Correll";
+  address: string = "3131 Plainfield Rd";
+  city: string = "Cincinnati";
+  state: string = "Ohio";
+  zip: string = "45237";
+  phoneNumber: number = 513-222-7878;
+  email: string = "kalicorrell@gmail.com";
+   workExperience: string[] = [
+    "Reinsurance Coordinator",  //| Ameritas |  2019 - 2020 Created Program to teach interns and new employees company processes.",
+    "Field and Licensing Representative", // | Ameritas | 2018 - 2019 | Improved processes to easily onboard new sales representatives.",
+    "First Impressions Coordinator", // | Matson Money | 2016 - 2018 | Created new filing system for whole company."
+   ];
+   education: string[] = [
+    "MAX Technical Training 12.2020"
+   ];
 }
